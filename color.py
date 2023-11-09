85bda588aca8d8a82ae191a63d17ab3c86f47bc2
Sameer Kongath <zoomixinfo@gmail.com>
2023-11-09 22:24:58 +0530

    First Commit

---
+++ color.py
@@ -0,0 +1,8 @@
+from colorama import Fore 
+
+print(Fore.RED + "ZOOMIXinfo.in")
+print(Fore.GREEN + "ZOOMIXinfo.in")
+print(Fore.YELLOW + "ZOOMIXinfo.in")
+print(Fore.BLUE + "ZOOMIXinfo.in")
+print(Fore.BLACK + "ZOOMIXinfo.in")
+print(Fore.MAGENTA + "ZOOMIXinfo.in")
